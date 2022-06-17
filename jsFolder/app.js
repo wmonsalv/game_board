f347e74adc3115ac31e3873ae04904e1cb70f589
William Monsalve <55903739+wmonsalv@users.noreply.github.com>
2022-06-16 23:30:05 -0400

    Almost finished
    Only thing left is to add the start over button.

---
+++ jsFolder/app.js
@@ -0,0 +1,57 @@
+
+let elementGrabber = document.getElementById("myId");
+let guest = document.getElementById("myId2");
+let count = 0;
+let count2 = 0;
+
+function addOne(){
+
+    count+=1;
+
+    elementGrabber.textContent = count;
+
+
+}
+
+function addTwo(){
+
+    count+=2;
+
+    elementGrabber.textContent = count;
+
+}
+
+function addThree(){
+
+    count+=3;
+
+    elementGrabber.textContent = count;
+
+}
+
+
+function addOne2(){
+
+    count2+=1;
+
+    guest.textContent = count2;
+
+
+}
+
+function addTwo2(){
+
+    count2+=2;
+
+    guest.textContent = count2;
+
+}
+
+function addThree2(){
+
+    count2+=3;
+
+    guest.textContent = count2;
+
+}
+
